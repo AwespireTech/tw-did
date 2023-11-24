cd0e60b2af64e77c89ff49f9853261711a0b566d
Foo Jia Wen <r10944057@csie.ntu.edu.tw>
2023-11-24 13:57:10 +0800

    add helmet and csp

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,29 @@
 import { Outlet } from '@tanstack/react-router';
 import { Header } from '@tw-did/react-library';
+import { Helmet } from 'react-helmet-async';
 // import { Header } from './libs';
 
 export function App() {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" http-equiv="Content-Security-Policy" content="
+          default-src 'self'; 
+          img-src 'self';
+          child-src 'none'; 
+          script-src 'self'; 
+          connect-src 'self' https://localhost:3000; 
+          style-src 'self' https://fonts.googleapis.com; 
+          font-src 'self' https://fonts.gstatic.com; 
+          object-src 'self'; 
+          base-uri 'self'; 
+          form-action 'self'; 
+          frame-ancestors 'self';  
+          frame-src 'self';
+          block-all-mixed-content;
+          upgrade-insecure-requests;
+        "/>
+      </Helmet>
       <Header path={window.location.pathname} />
       <Outlet />
     </div>
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,6 +4,7 @@ import * as ReactDOM from 'react-dom/client';
 import { RouterProvider } from '@tanstack/react-router';
 import { router } from './router';
 import { ApplicationContext } from './contexts/ApplicationContext';
+import { HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <StrictMode>
     <ApplicationContext>
-      <RouterProvider router={router} />
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
     </ApplicationContext>
   </StrictMode>
 );
